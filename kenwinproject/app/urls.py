99ac1d5b5c514141d1b5b616f000b34ee57aa437
Fortes <fortesa@spa1.gba.gov.ar>
2022-07-06 08:56:52 -0300

    add contact and remove

--- kenwinproject/app/urls.py
+++ kenwinproject/app/urls.py
@@ -4,5 +4,8 @@ from .views import ContactView
 
 urlpatterns = [
     path('', ContactView.contactList, name='contact_list'),
+    path('contact/addform', ContactView.contactAddForm, name='contact_addform'),
+    path('contact/add', ContactView.contactAdd, name='contact_add'),
+    path('contact/delete/<int:id>', ContactView.contactDelete, name='contact_delete'),
     path('contact/list', ContactView.contactList, name='contact_list'),
 ]
--- kenwinproject/app/views.py
+++ kenwinproject/app/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView
 from .models import Contact
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 class ContactView(ListView):
+    @login_required
+    def contactAdd(request):
+        Contact.objects.create(
+            firstname=request.POST['firstname'],
+            lastname=request.POST['lastname'],
+            enterprice=request.POST['enterprice'],
+            number=request.POST['number'],
+            email=request.POST['email'],
+            address=request.POST['address'],
+        )
+        messages.success(request, request.POST['firstname'] + ' ha sido creado')
+        return redirect('/contact/list')
+
+    @login_required
+    def contactAddForm(request):
+        return render(request, 'crud_contacts/contact_add.html')
+    
+    @login_required
+    def contactDelete(request, id):
+        contact = Contact.objects.get(id=id)
+        Contact.objects.filter(id=id).delete()
+        messages.success(request,  contact.firstname + ' ha sido eliminada')
+        return redirect('/contact/list')
+
     @login_required
     def contactList(request):
         if request.method == "POST":
